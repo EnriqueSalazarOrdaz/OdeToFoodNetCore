88d1063b6f8c27b15db74d52f109b2c8c8b4c1a2
enrique.salazar <enriquesa7@gmail.com>
2017-12-02 14:07:43 -0600

    add IGreeter services and create a class Greeter implementing IGreeter and return a Hello message

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,14 @@
+﻿namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessage();
+    }
+    public class Greeter : IGreeter
+    {
+        public string GetMessage()
+        {
+            return "Hello from Greeter services";
+        }
+    }
+}
