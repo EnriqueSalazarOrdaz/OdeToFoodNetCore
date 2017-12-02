8cfd3d60df399c52c35a7cf38f704598301f69c4
enrique.salazar <enriquesa7@gmail.com>
2017-12-02 14:33:57 -0600

    make more flexibility and testability service and split the logic using best practices.

--- OdeToFood/IGreeter.cs
+++ OdeToFood/IGreeter.cs
@@ -1,4 +1,6 @@
-﻿namespace OdeToFood
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
 {
     public interface IGreeter
     {
@@ -6,9 +8,16 @@
     }
     public class Greeter : IGreeter
     {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public string GetMessage()
         {
-            return "Hello from Greeter services";
+            //return "Hello from Greeter services";
+            return _configuration["Greeting"];
         }
     }
 }
