8051642e787a1a8aaf51f378b5f1431802550d69
enrique.salazar <enriquesa7@gmail.com>
2017-12-05 00:41:14 -0600

    add views folder-->home-->MVC view page, html snippet,pass the model to the view, then binding @model

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -8,7 +8,8 @@ namespace OdeToFood.Controllers
         public IActionResult Index()
         {
             var model = new Restaurant { Id=1,Name="kike's restaurant" };
-            return new ObjectResult(model);//return json(default), xml, csv, pdf, etc, according to the header http request.
+            //return new ObjectResult(model);//return json(default), xml, csv, pdf, etc, according to the header http request.
+            return View(model);
         }
     }
 }
