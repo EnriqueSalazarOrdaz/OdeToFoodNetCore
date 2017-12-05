eb2f753efba19d28bbafbaa206de26fc13def5b7
STKPROJECTS\enrique.salazar <enriquesa7@gmail.com>
2017-12-05 16:11:56 -0600

    addScoped services Irestaurant. Implementing at Ctor HomeController IRestaurant. change the way to get model restaurant. change type binding model(1-->many)at view.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Services;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        private IRestaurantData _restaurantData;
+
+        public HomeController(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
         public IActionResult Index()
         {
-            var model = new Restaurant { Id=1,Name="kike's restaurant" };
+            //var model = new Restaurant { Id=1,Name="kike's restaurant" };
             //return new ObjectResult(model);//return json(default), xml, csv, pdf, etc, according to the header http request.
+            var model = _restaurantData.GetAll();
             return View(model);
         }
     }
