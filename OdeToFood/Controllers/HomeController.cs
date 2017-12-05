3abd2b976d8b469fb2de315b31eb17b13304dc09
STKPROJECTS\enrique.salazar <enriquesa7@gmail.com>
2017-12-05 16:49:10 -0600

    add ViewModels. add HomeIndexViewModel with its arrange best practice. change the way tha we pass the model and change it for ViewModel. also use IGreeter services for the ViewModel. change again the binding model and repect changes on view.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
 using OdeToFood.Services;
+using OdeToFood.ViewModels.Home;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData
+                              ,IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
         public IActionResult Index()
         {
-            //var model = new Restaurant { Id=1,Name="kike's restaurant" };
+            //var model = new Restaurant { Id=1,Name="kike's restaurant" }; //using simple class
             //return new ObjectResult(model);//return json(default), xml, csv, pdf, etc, according to the header http request.
-            var model = _restaurantData.GetAll();
+            //var model = _restaurantData.GetAll(); //using a model
+            var model = new HomeIndexViewModel();     //using a viewModel
+            model.Restaurants = _restaurantData.GetAll(); //populate ViewModel using services that already existed
+            model.CurrentMessage = _greeter.GetMessage(); //populate ViewModel using services that already existed
             return View(model);
         }
     }
