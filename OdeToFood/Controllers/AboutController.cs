f7a4a5e086c1c3294892a617168b0ca1c79445c0
enrique.salazar <enriquesa7@gmail.com>
2017-12-05 00:04:27 -0600

    Route attribute [Route("About")], also using tokens at attribute(decorations)----[Route("[controller]/[action]")]

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -1,16 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
+    //[Route("myOwnPath/[controller]/[action]")]
+    [Route("[controller]/[action]")] //using tokens
+    //[Route("About")]
     public class AboutController
     {
+        //[Route("Phone")]//using text literal
+        //[Route("")]//when blank space, making like default action, kind of
         public string Phone()
         {
             return "1+555+555+555+555";
         }
+
+        //[Route("[action]")]//using token
         public string Address()
         {
             return "USA";
