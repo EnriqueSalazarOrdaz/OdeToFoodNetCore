2fc4390b334007f92d48b756382edbb58f837d6a
STKPROJECTS\enrique.salazar <enriquesa7@gmail.com>
2017-12-05 15:51:20 -0600

    display multiples restaurants: folder services..move IGreeter here. create interface IRestaurantData (crud operation). add inMemoryRestaurantData---kind of  entity afeter we will delete.

---
+++ OdeToFood/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private List<Restaurant> _restaurant;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurant = new List<Restaurant> {
+                new Restaurant { Id = 1, Name = "kike's restaurant" },
+                new Restaurant { Id = 2, Name = "Little caesar" },
+                new Restaurant { Id = 3, Name = "Chilis" }
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurant.OrderBy(r => r.Name);
+        }
+    }
+}
